c235d0f8135fa02fef6a123c8612625ce43cd08d
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2020-07-28 14:32:11 +0200

    id generator synced

--- src/repository/AmenityRepository.java
+++ src/repository/AmenityRepository.java
@@ -11,61 +11,65 @@ public class AmenityRepository {
 	private String path = "WebContent/db/amenities.json";
 	private LinkedList<Amenity> amenities = new LinkedList<Amenity>();
 	private Persistence<Amenity> persistance = new Persistence<Amenity>();
-	
+
 	private AmenityRepository() {
 		amenities = (LinkedList<Amenity>) findAll();
 	}
-	
+
 	public static AmenityRepository getInstance() {
-		if(instance == null)	instance = new AmenityRepository();
+		if (instance == null)
+			instance = new AmenityRepository();
 		return instance;
 	}
-	
-	public synchronized Collection<Amenity> findAll(){
+
+	public synchronized Collection<Amenity> findAll() {
 		LinkedList<Amenity> available = new LinkedList<Amenity>();
 		LinkedList<Amenity> allAmenities = persistance.read(path);
 		for (Amenity amenity : allAmenities) {
-			if(!amenity.isDeleted())	available.add(amenity);
+			if (!amenity.isDeleted())
+				available.add(amenity);
 		}
 		return available;
 	}
-	
-	public synchronized Amenity findById(int id){
+
+	public synchronized Amenity findById(int id) {
 		for (Amenity amenity : amenities) {
-			if(amenity.getId() == id)	return amenity;
+			if (amenity.getId() == id)
+				return amenity;
 		}
 		return null;
 	}
-	
+
 	public synchronized Amenity create(Amenity amenity) {
 		amenity.setId(getAvailableId());
 		amenities.add(amenity);
 		persistance.save(amenities, path);
 		return amenity;
 	}
-	
+
 	public synchronized void update(Amenity amenity) {
 		Amenity old = findById(amenity.getId());
-		if(old != null) {
+		if (old != null) {
 			amenities.set(amenities.indexOf(old), amenity);
 			persistance.save(amenities, path);
 		}
 	}
-	
+
 	public synchronized void delete(int id) {
 		Amenity deleting = findById(id);
-		if(deleting != null) {
+		if (deleting != null) {
 			ApartmentRepository.getInstance().deleteAmenityInApartments(deleting);
 			deleting.setDeleted(true);
 			update(deleting);
 		}
 	}
-	
-	private int getAvailableId() {
+
+	private synchronized int getAvailableId() {
 		int id = (amenities.size() != 0) ? (amenities.get(0).getId()) : 1;
 		for (Amenity amenity : amenities) {
-			if(amenity.getId() > id)	id = amenity.getId();
+			if (amenity.getId() > id)
+				id = amenity.getId();
 		}
-		return id +1;
+		return id + 1;
 	}
 }
--- src/repository/ApartmentRepository.java
+++ src/repository/ApartmentRepository.java
@@ -13,55 +13,57 @@ public class ApartmentRepository {
 	private String path = "WebContent/db/apartments.json";
 	private LinkedList<Apartment> apartments = new LinkedList<Apartment>();
 	private Persistence<Apartment> persistance = null;
-	
+
 	private ApartmentRepository() {
 		persistance = new Persistence<Apartment>();
 		apartments = persistance.read(path);
 	}
-	
+
 	public static ApartmentRepository getInstance() {
-		if(instance == null)	instance = new ApartmentRepository();
+		if (instance == null)
+			instance = new ApartmentRepository();
 		return instance;
 	}
-	
-	public synchronized Collection<Apartment> findAll(){
+
+	public synchronized Collection<Apartment> findAll() {
 		return apartments;
 	}
-	
+
 	public synchronized Apartment findById(int id) {
 		for (Apartment apartment : apartments) {
-			if(apartment.getId() == id)	return apartment;
+			if (apartment.getId() == id)
+				return apartment;
 		}
 		return null;
 	}
-	
+
 	public synchronized Apartment create(Apartment apartment) {
 		apartment.setId(getAvailableId());
 		apartments.add(apartment);
 		persistance.save(apartments, path);
 		return apartment;
 	}
-	
+
 	public synchronized void update(Apartment apartment) {
 		Apartment old = findById(apartment.getId());
-		if(old != null) {
+		if (old != null) {
 			apartments.set(apartments.indexOf(old), apartment);
-			System.out.println(apartments.indexOf(apartment) + " "+apartments.get(0).getAmenityIds().size());
+			System.out.println(apartments.indexOf(apartment) + " " + apartments.get(0).getAmenityIds().size());
 			persistance.save(apartments, path);
 		}
 	}
-	
+
 	public synchronized void delete(int id) {
 		Apartment deleting = findById(id);
-		if(deleting != null) {
+		if (deleting != null) {
 			deleting.setDeleted(true);
 			update(deleting);
 		}
 	}
-	
+
 	public synchronized void deleteAmenityInApartments(Amenity amenity) {
 		for (Apartment apartment : apartments) {
-			if(apartment.getAmenityIds().contains(amenity.getId())) {
+			if (apartment.getAmenityIds().contains(amenity.getId())) {
 				ArrayList<Integer> newList = apartment.getAmenityIds();
 				newList.remove(amenity.getId());
 				apartment.setAmenityIds(newList);
@@ -69,12 +71,13 @@ public class ApartmentRepository {
 			}
 		}
 	}
-	
-	private int getAvailableId() {
+
+	private synchronized int getAvailableId() {
 		int id = (apartments.size() != 0) ? (apartments.get(0).getId()) : 1;
 		for (Apartment apartment : apartments) {
-			if(apartment.getId() > id)	id = apartment.getId();
+			if (apartment.getId() > id)
+				id = apartment.getId();
 		}
-		return id +1;
+		return id + 1;
 	}
 }
