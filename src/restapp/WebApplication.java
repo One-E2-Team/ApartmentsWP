4ff24756ab21827658ba935f32d8054da1bc7792
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-08-04 19:13:00 +0200

    rest fix
    now, rest is only defined in web.xml

--- src/restapp/WebApplication.java
+++
@@ -1,9 +0,0 @@
-package restapp;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class WebApplication extends Application{
-
-}
--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,4 +1,4 @@
-package restapp.services;
+package services;
 
 import java.util.ArrayList;
 
