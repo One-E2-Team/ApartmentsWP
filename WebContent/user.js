519d38527bf14c5c4a423922489833bb557f9c5d
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-08-04 23:09:50 +0200

    logout

--- WebContent/user.js
+++ WebContent/user.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
   $("#login").submit(function (event) {
-      var username = $("input[name=loginUsername]").val();
-      var password = $("input[name=loginPassword]").val();
-      var userjson = JSON.stringify(new Administrator(username, password, "", "", "MALE", "GUEST", false));
+    var username = $("input[name=loginUsername]").val();
+    var password = $("input[name=loginPassword]").val();
+    var userjson = JSON.stringify(
+      new Administrator(username, password, "", "", "MALE", "GUEST", false)
+    );
     $.ajax({
       url: "rest/user/login",
       type: "POST",
@@ -10,11 +12,29 @@ $(document).ready(function () {
       contentType: "application/json",
       dataType: "json",
       complete: function (data) {
-          alert(1);
-        $("#result").html(data.responseText);
         console.log(data.responseText);
       },
     });
     event.preventDefault();
+  });
+});
+
+$(document).ready(function () {
+  $("#logout").click(function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: "rest/user/logout",
+      type: "GET",
+      data: "",
+      contentType: "application/json",
+      dataType: "json",
+      complete: function (data, status) {
+        if (data.responseText != "true" || status != "success") {
+          $("#ModalCenter").modal();
+        } else {
+          window.location.href = "index.html";
+        }
+      },
+    });
   });
 });
