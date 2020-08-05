2190e5c39ae8670e2280d6e64352e4a067c4ee16
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2020-08-06 01:31:16 +0200

    adding apartment added to completed tasks

--- WebContent/apartments.js
+++ WebContent/apartments.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     complete: function (data, status) {
       if (status == "success") {
         populateAmenities(JSON.parse(data.responseText));
+        $("#addApartmentElement").addClass("d-none");
       }
     },
   });
@@ -25,6 +26,7 @@ function populateAmenities(amenities) {
 $(document).ready(function () {
   $("#addApartmentForm").submit(function (event) {
     event.preventDefault();
+    $("#addApartmentError").addClass("d-none");
     let price = $("#pricePerNight").val();
     let zipcode = $("#zipcode").val();
     let city = $("#city").val();
@@ -34,13 +36,88 @@ $(document).ready(function () {
     let guestNum = $("#guestNum").val();
     let street = $("#street").val();
     let streetNum = $("#streetNum").val();
-    if(addApartmentErrorExist(price, zipcode, city, latitude, longitude, roomNum, guestNum, street, streetNum)) return;
-    alert("no errors");
+    if (
+      addApartmentErrorExist(
+        price,
+        zipcode,
+        city,
+        latitude,
+        longitude,
+        roomNum,
+        guestNum,
+        street,
+        streetNum
+      )
+    ) {
+      $("#addApartmentError").removeClass("d-none");
+      return;
+    }
+    let address = new Address(street, city, streetNum, zipcode);
+    let location = new Location(latitude, longitude, address);
+    let type = "APARTMENT";
+    if (document.getElementById("apartmentType").value == "SOBA") type = "ROOM";
+    let apartment = new Apartment(
+      0,
+      "INACTIVE",
+      false,
+      type,
+      parseInt(guestNum, 10),
+      parseInt(roomNum, 10),
+      location,
+      "username",
+      null,
+      price,
+      2,
+      10,
+      "PM",
+      "AM",
+      null, //TODO: get selected amenities
+      null,
+      null,
+      null,
+      null
+    );
+    let json = JSON.stringify(apartment);
+
+    $.ajax({
+      url: "rest/apartment/addApartment",
+      type: "POST",
+      data: json,
+      contentType: "application/json",
+      dataType: "json",
+      complete: function (data, status) {
+        if(status=="success"){
+          alert(69);
+        }
+      },
+    });
   });
 });
 
-function addApartmentErrorExist(price, zipcode, city, latitude, longitude, roomNum, guestNum, street, streetNum){
-  if (!price || !zipcode || !city || !latitude || !longitude || !roomNum || !guestNum || !street || !streetNum)
+function addApartmentErrorExist(
+  price,
+  zipcode,
+  city,
+  latitude,
+  longitude,
+  roomNum,
+  guestNum,
+  street,
+  streetNum
+) {
+  if (
+    !price ||
+    !zipcode ||
+    !city ||
+    !latitude ||
+    !longitude ||
+    !roomNum ||
+    !guestNum ||
+    !street ||
+    !streetNum ||
+    parseFloat(latitude) != latitude ||
+    parseFloat(longitude) != longitude
+  )
     return true;
   return false;
 }
--- src/services/ApartmentService.java
+++ src/services/ApartmentService.java
@@ -2,18 +2,24 @@ package services;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import beans.apartment.Amenity;
+import beans.apartment.Apartment;
+import beans.apartment.Comment;
 import beans.user.Role;
 import beans.user.User;
 import repository.AmenityRepository;
+import repository.ApartmentRepository;
 
 @Path("/apartment")
 public class ApartmentService {
@@ -23,13 +29,32 @@ public class ApartmentService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Collection<Amenity> getAllAmenities(@Context HttpServletRequest request) {
 		User user = (User) request.getSession().getAttribute("user");
-		if(user == null || user.getRole()==Role.GUEST) return null;
+		if (user == null || user.getRole() == Role.GUEST)
+			return null;
 		Collection<Amenity> ret = new ArrayList<Amenity>();
 		for (Amenity amenity : AmenityRepository.getInstance().getAll()) {
 			if (!amenity.isDeleted())
 				ret.add(amenity);
 		}
 		return ret;
+	}
+
+	@POST
+	@Path("/addApartment")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Apartment addApartment(@Context HttpServletRequest request, Apartment apartment) {
+		User user = (User) request.getSession().getAttribute("user");
+		if (user == null || user.getRole() != Role.HOST)
+			return null;
+		apartment.setHostId(user.getUsername());
+		apartment.setComments(new ArrayList<Comment>());
+		apartment.setAmenityIds(new ArrayList<Integer>());
+		apartment.setReservationIds(new ArrayList<Integer>());
+		apartment.setPicturePaths(new ArrayList<String>());
+		apartment.setRentableDates(new ArrayList<Date>());
+		apartment.setAvailableDates(new ArrayList<Date>());
+		return ApartmentRepository.getInstance().create(apartment);
 	}
 
 }
