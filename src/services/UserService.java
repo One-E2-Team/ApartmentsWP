f66ecfc3ee6e8cf0072925658f56fc06280c2832
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2020-08-05 15:02:34 +0200

    saving emtpy list instead of null for new guest

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -42,7 +42,7 @@ public class UserService {
 			request.getSession().invalidate();
 		return true;
 	}
-	
+
 	@GET
 	@Path("/test")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -57,11 +57,11 @@ public class UserService {
 	public User register(@Context HttpServletRequest request, Guest newUser) {
 		if ((User) request.getSession().getAttribute("user") != null)
 			request.getSession().invalidate();
-		User existing = UserRepository.getInstance().create(newUser);
-		if (existing == null)
-			return null; //TODO: proper error message
 		newUser.setRentedApartmentIds(new ArrayList<Integer>());
 		newUser.setReservationIds(new ArrayList<Integer>());
+		User existing = UserRepository.getInstance().create(newUser);
+		if (existing == null)
+			return null; // TODO: proper error message
 		request.getSession(true).setAttribute("user", newUser);
 		return newUser;
 	}
