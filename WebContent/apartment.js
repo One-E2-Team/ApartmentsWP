81244eabf630cad7dc1a040f4d327478d02ede19
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2020-08-09 15:44:30 +0200

    my pretty function is discarted

--- WebContent/apartment.js
+++ WebContent/apartment.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-  $.ajax({
-    url: "rest/apartment/getApartment?" + getQueryParams(),
-    type: "GET",
-    data: "",
-    dataType: "",
-    complete: function(data, status) {
-      if (status == "nocontent") {
-        alert("Pogrešni argumenti putanje");
-      }
-    },
-  });
+  if (location.search)
+    $.ajax({
+      url: "rest/apartment/getApartment" + location.search,
+      type: "GET",
+      data: "",
+      dataType: "",
+      complete: function(data, status) {
+        if (status == "nocontent") {
+          alert("Pogrešni argumenti putanje");
+        }
+      },
+    });
+  else alert("Niste uneli argument putanje!");
 });
